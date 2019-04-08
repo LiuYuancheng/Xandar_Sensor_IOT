8755d551d559b71d53f205f7305f7245b5856c12
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-04-08 18:07:56 +0800

    receive the sensor data and show the people number on the PI.

---
+++ TCP_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#-----------------------------------------------------------------------------
+# Name:        XAKAsensor reader.py
+#
+# Purpose:     This function is used to read the data from XAKA people counting
+#              sensor and show the data in the UI list.
+#
+# Author:      Yuancheng Liu
+#
+# Created:     2019/03/27
+# Copyright:   YC
+# License:     YC
+#-----------------------------------------------------------------------------
+
+import socket
+import time
+from sense_hat import SenseHat
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "request data."
+pIactiveFlag = True
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+# Init the sensHat contorl
+sense = SenseHat()
+sense.show_message('Ready')
+
+for i in range(10):
+    s.send(MESSAGE)
+    data = s.recv(BUFFER_SIZE)
+    time.sleep(1)
+    print ("received data:"+str(data))
+    Id, crt, avg = str(data).split('-')
+    sense.show_message('>'+str(avg), text_colour=(0,255,0))
+
+s.close()
