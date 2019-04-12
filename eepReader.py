0a0610d98b763315acfd55b495ae132d52e693b3
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-04-12 18:01:10 +0800

    Help program

---
+++ eepReader.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+subprocess.call('date')
+folderpath = os.getcwd()
+print("Current work directory is:"+str(folderpath))
+print("Read the eeprom data.")
+subprocess.call("sudo ./eepflash.sh -f=sense_read.eep -t=24c32 -r", shell=True)
+print("Convert the *.eep file")
+subprocess.call("./eepdump sense_read.eep eepData.txt",shell=True)
+startPrint = False
+with open('eepData.txt', 'r') as f:
+    for line in f:
+        if str(line).rstrip().strip() == 'dt_blob':
+            startPrint = True
+            subprocess.call('clear')
+        if startPrint: print(line)
+
+
