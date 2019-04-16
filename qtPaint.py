3ef6cd5b0f6ffc09597e5052daf51ae49fc43d0a
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-04-16 18:04:00 +0800

    Added more Paint method.

--- qtPaint.py
+++ qtPaint.py
@@ -2,8 +2,7 @@ import sys
 import random
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QWidget, QApplication)
-from PyQt5.QtGui import (QPainter, QColor, QFont)
-
+from PyQt5.QtGui import (QPainter, QColor, QFont, QPen, QBrush, QPainterPath)
 
 class PaintTest(QWidget):
 
@@ -12,7 +11,7 @@ class PaintTest(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 400, 300)
+        self.setGeometry(300, 300, 400, 600)
         self.text = 'color text test'
         self.setWindowTitle("PyQt paint test")
         self.show()
@@ -26,7 +25,12 @@ class PaintTest(QWidget):
         self.drawPoints(event, qp)
         # Draw shap and compare color
         self.drawRectangles(event, qp)
-
+        # Draw lines
+        self.drawLines(event, qp)
+        # Draw Brushes
+        self.drawBrushes(event, qp)
+        # Draw Curve
+        self.drawCurve(event, qp)
         qp.end()
 
     def drawText(self, event, qp):
@@ -55,6 +59,47 @@ class PaintTest(QWidget):
         qp.setBrush(QColor(25, 0, 90, 200))
         qp.drawRect(250, 15, 90, 60)
 
+    def drawLines(self, event, qp):
+        pen = QPen(Qt.blue, 2, Qt.SolidLine)
+        qp.setPen(pen)
+        y, offset = 80, 10
+        # Draw solidline
+        qp.setPen(pen)
+        qp.drawLine(20, y, 250, y)
+        y += offset
+        # Draw othe line
+        styleList = (Qt.DashLine, Qt.DashDotLine,
+                     Qt.DotLine, Qt.DashDotDotLine)
+        for sytleI in styleList:
+            pen.setStyle(sytleI)
+            qp.setPen(pen)
+            qp.drawLine(20, y, 250, y)
+            y += offset
+        # Draw customer line:
+        pen.setStyle(Qt.CustomDashLine)
+        pen.setDashPattern([1, 4, 5, 4])
+        qp.setPen(pen)
+        qp.drawLine(20, y, 250, y)
+
+    def drawBrushes(self, event, qp):
+        
+        brushesList = (Qt.SolidPattern, Qt.Dense1Pattern, Qt.Dense2Pattern, Qt.DiagCrossPattern,\
+                    Qt.Dense5Pattern, Qt.Dense6Pattern, Qt.HorPattern, Qt.VerPattern, Qt.BDiagPattern)
+        for idx, item in  enumerate(brushesList):
+            x = 10 + (idx % 3)*100
+            y = 400 + (idx//3)*70
+            brush = QBrush(item)
+            qp.setBrush(brush)
+            qp.drawRect(x, y, 90, 60)
+
+    def drawCurve(self, event, qp):
+        path = QPainterPath()
+        # reset the pen and brush to default.
+        qp.setPen(QPen())
+        qp.setBrush(QBrush())
+        path.moveTo(30, 30)
+        path.cubicTo(30, 30, 200, 350, 350, 30)
+        qp.drawPath(path)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
