9cf0dddc768721f4c5838a948bf437a231d14c08
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-05-13 09:58:09 +0800

    Changed all the create_XX message to private.

--- firmwSign/firmwMsgMgr.py
+++ firmwSign/firmwMsgMgr.py
@@ -2,7 +2,7 @@
 # Name:        firmwMsgMgr.py
 #
 # Purpose:     This module is used create a message manager to dump the user 
-#              message to a json and Bytes data. 
+#              message to a json string/bytes data and load back to orignal data. 
 # Author:      Yuancheng Liu
 #
 # Created:     2019/05/09
@@ -13,24 +13,25 @@ import os
 import json
 import time
 
-CMD_TYPE = 'C'.encode('utf-8')  # cmd type message used for contorl.  
+CMD_TYPE = 'C'.encode('utf-8')  # cmd type message used for contorl.
 FILE_TYPE = 'F'.encode('utf-8') # file(bytes) type.
-RAN_LEN = 4
-
+RAN_LEN = 4 # The random number/bytes length.
 # Action type:
 # CR    - Connection request
 # HB    - Heart beat (feedback)
-# LI1   - Login step 1 [Username + random1]
-# LR1   - Login response 1 [random1 + random2]
-# LI2   - login step2 [random2 + password]
-# LR2   - login response 2 [Challenge for SWATT]
-# CF    - certificate Fetch.    
-# SR    - signature response
+# LI1   - Login request step 1 [Username + random1(client->sever)]
+# LR1   - Login response 1 [random1 + random2(client<-server)]
+# LI2   - Login request step2 [random2 + password]
+# LR2   - Login response 2 [Challenge for SWATT]
+# CF    - Certificate file fetch.    
+# SR    - Signature response
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 class msgMgr(object):
-    
+    """ Create a message manager to dump the user message to a json string/bytes
+        data and load back to orignal data.
+    """
     def __init__(self, parent):
         self.parent = parent
         self.data = None
@@ -41,58 +42,66 @@ class msgMgr(object):
         """
         datab = None
         if action == 'CR':
-            datab = self.createCRmsg()
+            datab = self._createCRmsg()
         elif action == 'HB':
             lastAct, state = dataArgs
-            datab = self.createHBmsg(lastAct, state)
+            datab = self._createHBmsg(lastAct, state)
         elif 'LI' in action:
             #print(action)
-            datab = self.createLImsg(args=dataArgs)
+            datab = self._createLImsg(args=dataArgs)
         elif 'LR' in action:
-            datab = self.createLRmsg(dataArgs)
+            datab = self._createLRmsg(dataArgs)
         elif action == 'CF':
-            datab = self.createCFmsg()
+            datab = self._createCFmsg()
         elif action == 'FL':
-            datab = self.createFLmsg(dataArgs)
+            datab = self._createFLmsg(dataArgs)
         elif action == 'SR':
-            datab = self.createSRmsg(dataArgs)
+            datab = self._createSRmsg(dataArgs)
         return datab
 
 #-----------------------------------------------------------------------------
-    def createCRmsg(self):
-        """ Create the connection response message.
+    def loadMsg(self, msg):
+        """ Convert the dumpped message back to orignal data.
+        """
+        tag = msg[0:1] # Take out the tag data.
+        data = json.loads(msg[1:]) if tag == CMD_TYPE else msg[1:]
+        return data
+
+#-----------------------------------------------------------------------------
+    def _createCRmsg(self):
+        """ Create the connection request message.
         """
         msgDict = {
             "act"   : 'CR',
             "time"  : time.time()
         }
-        tag = CMD_TYPE
-        data = json.dumps(msgDict).encode('utf-8')
-        return tag + data
+        return CMD_TYPE + json.dumps(msgDict).encode('utf-8')
 
 #-----------------------------------------------------------------------------
-    def createHBmsg(self, lastAct, state):
+    def _createHBmsg(self, lastAct, state):
+        """ Create a heart beat function to handle the cmd execution response.
+        """
         msgDict = {
             "act"   : 'HB',
             "lAct"  : lastAct,  # last received action 
-            "state" : state     # last action exe state. 
+            "state" : state     # last action exe state.(0/1)
         }
-        tag = CMD_TYPE
-        data = json.dumps(msgDict).encode('utf-8')
-        return tag + data
+        return CMD_TYPE + json.dumps(msgDict).encode('utf-8')
 
 #-----------------------------------------------------------------------------
-    def createLImsg(self, args = None):
-        #print(args)
-        if args is None:
-            return None 
-        if len(args) == 1:
-            userName = args[0]
+    def _createLImsg(self, args=None):
+        """ Create a login message: 
+            login step1: send userName + randomNum1
+            login step2: send randomNum2 + password
+        """ 
+        if args is None: return None 
+        if isinstance(args, str):
+            userName = args.strip()  # remove the user space.
             randomB = os.urandom(RAN_LEN)
             msgDict = {
-                "act"       : 'LI1',
-                "user"      : str(userName),
-                "random1"   : randomB.hex() 
+                "act"   : 'LI1',
+                "user"  : str(userName),
+                "random1": randomB.hex()
             }
             data = CMD_TYPE + json.dumps(msgDict).encode('utf-8')
             return (data, randomB)
@@ -107,15 +116,17 @@ class msgMgr(object):
             return data
 
 #-----------------------------------------------------------------------------
-    def createLRmsg(self, args = None):
-        if len(args) == 1:
-            challenge = args[0]
+    def _createLRmsg(self, args=None):
+        """ Create a login request.
+        """ 
+        if args is None: return args
+        if isinstance(args, str):
+            challenge = args.strip()
             msgDict = {
                 "act"       : 'LR2',
                 "challenge" : challenge,
             }
-            data = CMD_TYPE + json.dumps(msgDict).encode('utf-8')
-            return data
+            return CMD_TYPE + json.dumps(msgDict).encode('utf-8')
         elif len(args) == 2:
             (randomB, state) = args
             randomB2 = os.urandom(RAN_LEN)
@@ -129,46 +140,40 @@ class msgMgr(object):
             return (data, randomB2)
 
 #-----------------------------------------------------------------------------
-    def createCFmsg(self):
+    def _createCFmsg(self):
+        """ Create a certificate file fetch requset.
+        """
         msgDict = {
             "act"   : 'CF',
             "time"  : time.time()
         }
-        tag = CMD_TYPE
-        data = json.dumps(msgDict).encode('utf-8')
-        return tag + data
+        return CMD_TYPE + json.dumps(msgDict).encode('utf-8')
 
 #-----------------------------------------------------------------------------
-    def createSRmsg(self, args):
-        sensorId, signerId,swatt , date, typeS, versionS, signS = args
+    def _createSRmsg(self, args):
+        """ Create a sign response message. 
+        """
+        if len(args) != 7:
+            print("The required message in the message list missing<%s>" %str(args))
+            return None
+        sensorId, signerId, swatt, date, typeS, versionS, signS = args
         msgDict = {
-            "act"   : 'SR',
-            "id"    : sensorId,
-            "sid"   : signerId,
-            "swatt" : swatt,
-            "date"  : date,
-            "tpye"  : typeS,
-            "version": versionS,
-            "signStr": signS.hex()
+            "act"   : 'SR',     
+            "id"    : sensorId,     # sensor ID
+            "sid"   : signerId,     # Signer factory user ID.
+            "swatt" : swatt,        # File SWATT value. 
+            "date"  : date,         # time stamp.
+            "tpye"  : typeS,        # Sensor type.
+            "version": versionS,    # Sensor version.
+            "signStr": signS.hex()  # Signature string.
         }
-        tag = CMD_TYPE
-        data = json.dumps(msgDict).encode('utf-8')
-        return tag + data
+        return CMD_TYPE + json.dumps(msgDict).encode('utf-8')
 
 #-----------------------------------------------------------------------------
-    def createFLmsg(self, bytesData):
-        tag = FILE_TYPE
-        return tag + bytesData
-
-#-----------------------------------------------------------------------------
-    def loadMsg(self, msg):
-        tag = msg[0:1]
-        if tag == CMD_TYPE:
-            data = msg[1:]
-            return json.loads(data)
-        elif tag == FILE_TYPE:
-            data = msg[1:]
-            return data
+    def _createFLmsg(self, bytesData):
+        """ Create the file message.
+        """
+        return FILE_TYPE + bytesData
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
