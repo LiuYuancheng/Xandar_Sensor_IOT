5421c436c382292c36ea11ed5184beca2bd80ffa
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-08-02 09:32:33 +0800

    changed version

--- exeSensor.bat
+++ exeSensor.bat
@@ -1,2 +1,2 @@
-"C:/Users/dcslyc/AppData/Local/Programs/Python/Python37-32/python.exe" "c:/Singtel/Programs/IOT/IOT/IOT/firmwSign/XAKAsensorRd.py"
+"C:/Users/dcslyc/AppData/Local/Programs/Python/Python37-32/python.exe" "c:/Singtel/Programs/IOT/IOT/IOT/firmwSign/XAKAsensorRd_v1.py"
 pause
